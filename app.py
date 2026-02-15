775667f87a11964f98e7f036d0b8d9bb607f9d01
JayRocketMaN <odidikasomto@gmail.com>
2026-02-15 18:29:32 +0100

    temporary_route_adjusment

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ bootstrap = Bootstrap5(myWebsite)
 
 @myWebsite.route("/")
 def home():
-    return render_template('home.html')
+    return render_template('about.html')#remeber to correct the route to the actual 'home' route
 
 @myWebsite.route("/skills")
 def skills():
@@ -18,4 +18,6 @@ def about():
     return render_template('about.html')
 
 if __name__ == "__main__":
-    myWebsite.run(debug=True,host="0.0.0.0",port=8090)
+    myWebsite.run(debug=True)
+
+    #host="0.0.0.0",port=8090
